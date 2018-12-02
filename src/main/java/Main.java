06f5826635d9943818ab2553174421ef34bd4c16
Martin Beránek <martin.beranek@embedit.cz>
2018-12-02 18:20:37 +0100

    Day 1 - part 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -14,8 +16,16 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Integer sum = 0;
-        for (Integer i : input()) {
-            sum += i;
+        Set<Integer> sums = new HashSet<>();
+        outer : while (true) {
+            for (Integer i : input()) {
+                sum += i;
+                if (sums.contains(sum)) {
+                    break outer;
+                } else {
+                    sums.add(sum);
+                }
+            }
         }
         System.out.println(sum);
     }
